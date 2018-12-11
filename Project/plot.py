833c1f322e4b242d8649098f784ece7f80dc09ae
bobvandenhoogen <bobvandenhoogen@hotmail.com>
2018-12-11 12:48:08 +0100

    plot legend update

--- Project/plot.py
+++ Project/plot.py
@@ -24,6 +24,7 @@ for env, directories in enumerate([dirs_CP, dirs_MC]):
     # f.set_figheight(5)
     # f.set_figwidth(5)
     exp_replay = {'off': axes[0, 0], 'prioritized': axes[0,1], 'uniform': axes[1,0]}
+    coloring = {'off': 'r', 'soft': 'b', 'hard': 'g'}
     lines = {}
     errors_upper = {}
     errors_lower = {}
@@ -49,12 +50,24 @@ for env, directories in enumerate([dirs_CP, dirs_MC]):
         for item in exp_replay.keys():
             if x.endswith(item):
                 result = re.search('_(.*)_', x).group(1)
-                exp_replay[item].plot(lines[x], label=result)
+                exp_replay[item].plot(lines[x], label=result, color=coloring[result])
                 exp_replay[item].set_title(item)
                 # print(runs_lower)
                 exp_replay[item].fill_between(range(len(lines[x])), errors_lower[x], errors_upper[x], alpha=0.15)
-                exp_replay[item].legend(loc=2)
-    axes[1,1].set_visible(False)
+                exp_replay[item].legend()
+                handles, labels = exp_replay[item].get_legend_handles_labels()
+                # sort both labels and handles by labels
+                labels, handles = zip(*sorted(zip(labels, handles), key=lambda t: t[0]))
+                exp_replay[item].legend(handles, labels, loc=2)
+
+    axes[1, 1].set_visible(False)
+    # for i in range(2):
+    #     for j in range(2):
+    #         handles, labels = axes[i, j].get_legend_handles_labels()
+    #     # sort both labels and handles by labels
+    #     labels, handles = zip(*sorted(zip(labels, handles), key=lambda t: t[0]))
+    #     ax.legend(handles, labels)
+
     plt.savefig('./images/' + envs[env], dpi=600)
 
 
